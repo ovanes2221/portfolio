8a9e20b33ad31c6c59a547e0729b3e07c95fa452
ovanes2221 <71893458+ovanes2221@users.noreply.github.com>
2020-10-03 11:59:41 +0300

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,13 @@
+let commentForm = document.querySelector('.comment-form');
+let commentList = document.querySelector('.comment-list');
+// Объявите переменную здесь
+
+commentForm.onsubmit = function (evt) {
+  evt.preventDefault();
+
+  let newComment = document.createElement('li');
+  newComment.classList.add('user-comment');
+  newComment.textContent = 'Новый комментарий';
+
+  commentList.append(newComment);
+};
